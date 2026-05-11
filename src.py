999a05a8deb0e12aafd56b89c90ff818d0196993
megans5 <megan.r.smith55@gmail.com>
2026-05-11 17:47:10 -0500

    add removal of columns outside of range, fix date range in config, add error logging to type conversion

--- src.py
+++ src.py
@@ -35,9 +35,14 @@ def quality_check(df, quality_check_col, logger):
     else:
         logger.log("There is no quality check column.")
 
+    return df
+
 def correct_types(df, type_by_col, logger):
 
-    logger.step("Type Correction", "Check the type of each column and correct it if inconsistent with type from config")
+    logger.step("Type Correction and Range Checking", "Check the type of each column and " \
+    "correct it if inconsistent with type from config. Remove any rows with values outside of range specified")
+
+    rows_removed_total = 0
     # iterate through the specified type information for each column from config
     for column_name, type_info in type_by_col.items():
         # check if range is included in the info
@@ -57,8 +62,9 @@ def correct_types(df, type_by_col, logger):
                 # convert the column to datetime type
                 df[column_name] = pd.to_datetime(df[column_name], errors="coerce")
                 logger.change(column_name, "Type: " + str(old_type), str(df[column_name].dtype))
+             # log an error if conversion cannot be made
             except Exception as e:
-                print("couldn't convert + " + column_name)
+                logger.error(column_name, f"could not convert from {str(old_type)} to {col_type}")
         # If the current data type of the column does not match what it should be and it shouldn't be a date
         elif df[column_name].dtype != col_type:
             try:
@@ -66,9 +72,30 @@ def correct_types(df, type_by_col, logger):
                 # convert the column to whatever is specified by the config
                 df[column_name] = df[column_name].astype(col_type)
                 logger.change(column_name, "Type: " + str(old_type), col_type)
+            # log an error if conversion cannot be made
             except Exception as e:
-                print(" wow! couldn't convert + " + column_name)
+                logger.error(column_name, f"could not convert from {str(old_type)} to {col_type}")
+        if lower_range and upper_range:
+            num_rows_before = len(df)
+            
+            # remove any rows that have values below the lower range or above upper range 
+            df = df[~(df[column_name]<lower_range)] 
+            df = df[~(df[column_name]>upper_range)] 
+            
+            # log the new number of rows and the number of rows removed if any rows were removed
+            num_rows_after = len(df)
+            difference = num_rows_before - num_rows_after
+            if difference > 0:
+                logger.log(f"\tFor range check column {column_name}: \n\t\t\tBefore removal number of rows: {num_rows_before}")
+                logger.log(f"\t\tAfter removal number of rows: {num_rows_after}")
+                logger.log(f"\t\tTotal number of rows removed: {num_rows_before - num_rows_after}")
+                # add number of rows removed to the tracker of rows removed
+                rows_removed_total = rows_removed_total + (num_rows_before - num_rows_after)
         
+    logger.log(f"Total number of rows removed for entire range check: {rows_removed_total}")
+    return df
+
+
 def main():
     logger = CurationLogger.CurationLogger("curation_log.txt")
     logger.step("Begin Curation")
@@ -77,8 +104,8 @@ def main():
     print(df.shape)
     for col in df.columns:
         print(col + "  ")
-    quality_check(df, quality_check_col, logger)
-    correct_types(df, type_by_cols, logger)
+    df = quality_check(df, quality_check_col, logger)
+    df = correct_types(df, type_by_cols, logger)
 
 
     logger.close()
