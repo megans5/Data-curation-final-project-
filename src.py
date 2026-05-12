76970f89501366929c644c2de5ce1161e526b8b5
megans5 <megan.r.smith55@gmail.com>
2026-05-12 17:37:27 -0500

    add removal of duplicates, add plotting of number of rows deleted, add removal of null values

--- src.py
+++ src.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 # import numpy as np
 import CurationLogger
 
@@ -12,14 +13,17 @@ def load_dataset(config_path):
     column_names = config.get("columnNames")
     type_by_col = config.get("typeByCol")
     quality_check_col = config.get("qualityCheckCol")
+    null_vals = config.get("nullVals")
+    id_col = config.get("idCol")
 
     # create a dataframe from the csv using only the specified columns
     df = pd.read_csv(file_path, encoding="cp1252", usecols=column_names)
 
-    return df, type_by_col, quality_check_col
+    return df, type_by_col, quality_check_col, null_vals, id_col
 
-def quality_check(df, quality_check_col, logger):
+def quality_check(df, quality_check_col, logger, removal_dict):
     logger.step("Quality check", "If a quality column is provided, remove rows which failed that check")
+    rows_removed_total = 0
     if quality_check_col:
         # iterate through all of the quality check column items 
         for column_name, values in quality_check_col.items():
@@ -32,12 +36,59 @@ def quality_check(df, quality_check_col, logger):
             num_rows_after = len(df)
             logger.log(f"\t\tAfter removal number of rows: {num_rows_after}")
             logger.log(f"\t\tTotal number of rows removed: {num_rows_before - num_rows_after}")
+            rows_removed_total = rows_removed_total + (num_rows_before - num_rows_after)
     else:
         logger.log("There is no quality check column.")
 
-    return df
+    removal_dict['Quality Check'] = rows_removed_total
+    return df, removal_dict
 
-def correct_types(df, type_by_col, logger):
+def remove_null(df, null_vals, logger, removal_dict):
+    logger.step("Remove null", "Remove all null values")
+    rows_removed_total = 0
+    for column_name in df.columns:
+        num_rows_before = len(df)
+        # remove all rows with actually null variables
+        df = df[~df[column_name].isna()]
+
+        # remove all rows which are empty strings
+        df = df[~df[column_name].astype(str).eq("")]
+        
+        # if there's a specific string or list of strings that means null, remove it
+        if null_vals:
+            if isinstance(null_vals, list):
+                for null_val in null_vals:
+                    df = df[~((df[column_name]).astype(str).str.lower().str.contains(null_val))]
+            else:
+                df = df[~((df[column_name]).astype(str).str.lower().str.contains(null_vals))]
+        num_rows_after = len(df)
+       # log number of rows removed
+        difference = num_rows_before - num_rows_after
+        if difference > 0:
+            logger.log(f"\tFor null removal on column {column_name}: \n\t\t\tBefore removal number of rows: {num_rows_before}")
+            logger.log(f"\t\tAfter removal number of rows: {num_rows_after}")
+            logger.log(f"\t\tTotal number of rows removed: {num_rows_before - num_rows_after}")
+            # add number of rows removed to the tracker of rows removed
+            rows_removed_total = rows_removed_total + (num_rows_before - num_rows_after)
+        
+    logger.log(f"Total number of rows removed: {rows_removed_total}")
+    removal_dict['Remove Null Values'] = rows_removed_total
+    return df, removal_dict
+
+def remove_duplicates(df, id_col, logger, removal_dict):
+    logger.step("Remove duplicate values", f"Remove all duplicate values based on the {id_col} column")
+    num_rows_before = len(df)
+    logger.log(f"\tBefore removal of duplicates: {num_rows_before} rows")
+    
+    # get rid of any rows that have the same id column (which should be unique)
+    df = df.drop_duplicates(subset=id_col)
+    logger.log(f"\tAfter removal of duplicates: {len(df)} rows")
+    logger.log(f"\tTotal number of rows removed: {num_rows_before - len(df)}")
+    removal_dict['Remove Duplicates'] = num_rows_before - len(df)
+    return df, removal_dict
+    
+
+def correct_types(df, type_by_col, logger, removal_dict):
 
     logger.step("Type Correction and Range Checking", "Check the type of each column and " \
     "correct it if inconsistent with type from config. Remove any rows with values outside of range specified")
@@ -93,21 +144,41 @@ def correct_types(df, type_by_col, logger):
                 rows_removed_total = rows_removed_total + (num_rows_before - num_rows_after)
         
     logger.log(f"Total number of rows removed for entire range check: {rows_removed_total}")
-    return df
+    # removal_dict['Remove Out of Range Values'] = rows_removed_total
+    removal_dict['Remove Out of Range'] = 0
+    return df, removal_dict
+
+def plot_graphs(df, removal_dict):
+    # turn the dict into df so it can be plotted and rename columns for graph 
+    removal_df = pd.DataFrame(list(removal_dict.items()), columns = ["Curation Step", "Number of Rows Removed"])
+    removal_df.plot.bar(x="Curation Step", y="Number of Rows Removed", color=None)
+    # plot rows removed by curation step
+    plt.ylabel("Number of Rows Removed")
+    plt.xlabel("Curation Step")
+    plt.title("Rows Removed by Curation Step")
+    plt.xticks(rotation=13, ha='right')
+    plt.savefig("removal_bar_graph.png")
+    # plt.show()
 
 
 def main():
     logger = CurationLogger.CurationLogger("curation_log.txt")
     logger.step("Begin Curation")
-    df, type_by_cols, quality_check_col = load_dataset("config.json")
-
-    print(df.shape)
+    df, type_by_cols, quality_check_col, null_val, id_col = load_dataset("config.json")
+    df.to_csv("raw_data.csv", index=False)
+    removal_dict = {}
+    num_rows_before = len(df)
     for col in df.columns:
         print(col + "  ")
-    df = quality_check(df, quality_check_col, logger)
-    df = correct_types(df, type_by_cols, logger)
+    df, removal_dict = quality_check(df, quality_check_col, logger, removal_dict)
+    df, removal_dict = remove_null(df, null_val, logger, removal_dict)
+    df, removal_dict = remove_duplicates(df, id_col, logger, removal_dict)
+    df, removal_dict = correct_types(df, type_by_cols, logger, removal_dict)
 
+    df.to_csv("curated_data.csv", index=False)
 
+    plot_graphs(df, removal_dict)
+    logger.step("Report", f"Before curation, the data had {num_rows_before} rows. After curtion, it has {len(df)} rows. {num_rows_before - len(df)} rows were removed.")
     logger.close()
 
 main()
